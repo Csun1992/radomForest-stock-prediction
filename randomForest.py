fb8f1d4426b3153f207f8f33714d13c13aab6c26
Chong Sun <sonshicyu@gmail.com>
2018-12-23 00:42:43 -0600

    Build random forest classifier object inheriting from svm object

---
+++ randomForest.py
@@ -0,0 +1,30 @@
+from svm import StockPrediction, StockPredNoClassification
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+
+class RandomForest(StockPredNoClassification):
+    def __init__(self, microDataLoc, estimators=55, depth=7):
+        StockPredNoClassification.__init__(self, microDataLoc)
+        self.estimators = estimators
+        self.depth = depth
+
+    def train(self):
+        train, test, trainLabel, testLabel = self.trainTestSplit()
+        clf = [RandomForestClassifier(n_estimators=self.estimators, max_depth=self.depth, random_state=41) for i in range(self.clusterNum)]
+        for i in range(self.clusterNum):
+            clf[i].fit(train[i], trainLabel[i])
+        return (clf, test, testLabel) # return test and testLabel to self.test() so no need to
+                                      # recompute the testing data again
+
+if __name__ == "__main__":
+    
+    depth = map(int, np.linspace(2, 10, 4))
+    estimators = map(int, np.linspace(5, 200, 20))
+
+    for i in depth:
+        for j in estimators:
+            print 'for depth = %s and estimators = %s'%(i, j)
+            apple = RandomForest("data/appleTrainData.txt", j, i)
+            apple.reportResult()
+            print '\n'
+
